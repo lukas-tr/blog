5949b6786ed1b07988dc289925175a2f1318a0cd
Lukas Troyer <36337818+lukas-tr@users.noreply.github.com>
2019-10-02 17:30:20 +0200

    add recaptcha

--- src/templates/contact.js
+++ src/templates/contact.js
@@ -12,12 +12,12 @@ export default class Contact extends React.Component {
               <header className="post-header">
                 <h1 className="post-title underline">{_.get(this.props, 'pageContext.frontmatter.title')}</h1>
               </header>
-              {_.get(this.props, 'pageContext.frontmatter.subtitle') && 
+              {_.get(this.props, 'pageContext.frontmatter.subtitle') &&
               <div className="post-subtitle">
                 {htmlToReact(_.get(this.props, 'pageContext.frontmatter.subtitle'))}
               </div>
               }
-              {_.get(this.props, 'pageContext.frontmatter.img_path') && 
+              {_.get(this.props, 'pageContext.frontmatter.img_path') &&
               <div className="post-thumbnail">
                 <img src={safePrefix(_.get(this.props, 'pageContext.frontmatter.img_path'))} alt={_.get(this.props, 'pageContext.frontmatter.title')} />
               </div>
@@ -25,7 +25,7 @@ export default class Contact extends React.Component {
               <div className="post-content">
                 {htmlToReact(_.get(this.props, 'pageContext.html'))}
                 <form name="contactForm" method="POST" netlifyHoneypot="bot-field" data-netlify="true" id="contact-form"
-                  className="contact-form">
+                  className="contact-form" data-netlify-recaptcha="true">
                   <p className="screen-reader-text">
                     <label>Don't fill this out if you're human: <input name="bot-field" /></label>
                   </p>
@@ -41,6 +41,10 @@ export default class Contact extends React.Component {
                     <label className="form-label">Message *</label>
                     <textarea name="message" placeholder="Your message..." className="form-textarea" rows="7" />
                   </p>
+                  <div className="form-row">
+                    <div data-netlify-recaptcha="true"></div>
+                  </div>
+                  <p></p>
                   <input type="hidden" name="form-name" value="contactForm" />
                   <p className="form-row">
                     <button type="submit" className="button">Send Message</button>
