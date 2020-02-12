706a3baf18f4478410a441654d37379ff09ccd07
Lukas Troyer <36337818+lukas-tr@users.noreply.github.com>
2020-02-12 17:44:03 +0100

    add rss feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,6 +77,59 @@ module.exports = {
         pageContextProperty: `menus`,
         menus: require("./src/data/menus.json")
       }
-    }
+    },
+ {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.url,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.url,
+                  custom_elements: [{ "content:encoded": edge.node.html }],
+                })
+              })
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields { url }
+                      frontmatter {
+                        title
+                        date
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "Lukas Troyer's Blog",
+          },
+        ],
+      },
+    },
   ]
 };
